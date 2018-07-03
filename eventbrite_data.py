afd17ab5bf0c9c9955815a1d1c06054cc55b04b5
Cierra Higgins <cierramhiggins@gmail.com>
2018-07-03 15:47:06 -0700

    parsed JSON data from Eventbrite

--- eventbrite_data.py
+++ eventbrite_data.py
@@ -1,5 +1,8 @@
 """ Data from Eventbrite's API """
 
+import requests
+import unicodedata
+
 
 # data = "https://www.eventbriteapi.com/v3/events/17920884849/?token=ANNKEY"
 # price = free
@@ -8,8 +11,47 @@
 # https://www.eventbriteapi.com/v3/events/search/q=free-events-in-san-francisco/?token=GN4T6AHNFWXAD6BRF4WZ
 # url = https://www.eventbriteapi.com/v3/events/search/?q=free+events+in+san+francisco&token=ZAKXWP2CDSBC2SS7RVYQ
 
+# next month
 # https://www.eventbriteapi.com/v3/events/search/?location.address=san+francisco&location.within=30mi&price=free&start_date.keyword=next_month&token=ZAKXWP2CDSBC2SS7RVYQ
+
+# next week
+# https://www.eventbriteapi.com/v3/events/search/?location.address=san+francisco&location.within=30mi&price=free&start_date.keyword=next_week&token=ZAKXWP2CDSBC2SS7RVYQ
 
 #free events bay area month of July
 
-url = "https://www.eventbriteapi.com/v3/events/search/?location.address=san+francisco&location.within=30mi&price=free&start_date.keyword=next_month&token={mytoken}".format(mytoken=mytoken)
+# url = "https://www.eventbriteapi.com/v3/events/search/?location.address=san+francisco&location.within=30mi&price=free&start_date.keyword=next_month&token={mytoken}".format(mytoken=mytoken)
+
+data = requests.get("https://www.eventbriteapi.com/v3/events/search/?location.address=san+francisco&location.within=30mi&price=free&start_date.keyword=next_week&token=ZAKXWP2CDSBC2SS7RVYQ")
+
+all_events = data.json()
+
+week_events = all_events['events']
+
+content = []
+
+for i in range(len(week_events)):
+        title = (week_events[i].get('name')['text'] or " ").encode('utf-8')
+        about = (week_events[i].get('description')['text'] or " ").encode('utf-8')
+        url = (week_events[i].get('url') or " ").encode('utf-8')
+        time = (week_events[i].get('start')['local'] or " ").encode('utf-8')
+        event = "title: {} description: {} url: {} time: {}".format(title, about, url, time)
+        event = unicode(event, 'utf-8')
+        content.append(event)
+# print title + "\n", "description: " + about + "\n", "url: " + url + "\n", "time: " + time
+# content = unicode(content)
+# final_events = content.normalize("NFKD", u'\xa0')
+
+content = str(content)
+content = content.split(" ")
+
+for item in content:
+    if '\\xa0' == item:
+        item.replace('\\xa0', ' ')
+    elif '\\n' == item:
+        item.replace('\\n', ' ')
+    elif '\\n' == item:
+        item.replace('\n', ' ')
+    elif '\\n' == item:
+        item.replace('\xa0', ' ')
+
+print " ".join(content)
