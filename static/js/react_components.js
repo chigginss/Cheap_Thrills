c8a9102b1f5a5c86cd701286cac8ff1f62b4938c
Cierra Higgins <cierramhiggins@gmail.com>
2018-07-13 14:03:22 -0700

    react components

---
+++ static/js/react_components.js
@@ -0,0 +1,28 @@
+
+
+class Cheap_Thrills_App extends React.Component {
+    render () {
+        const title = "Cheap Thrills"
+        const subtitle = "Free Events in the Bay Area"
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+            </div>
+        );
+
+    }
+
+}
+
+
+class Header extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+      </div>
+    );
+  }
+
+}
